3148d6705c0a93f5dc52efbd844ad5d91027e022
Peter <peter.c.schueke@gmail.com>
2013-08-23 14:15:46 -0700

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('peterschueke.github.comApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
